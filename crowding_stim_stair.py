eec8a3223c4246f866d7f4e2ed7354b9b56fde7a
Inês Veríssimo <verissimo.iness@gmail.com>
2019-04-02 15:06:40 +0200

    calling params from json, added sys arguments to run from terminal, staircase only within each block (keep or change?)

--- crowding_stim_stair.py
+++ crowding_stim_stair.py
@@ -14,15 +14,19 @@ varying distance between target and flankers with staircase method
 
 from psychopy import visual, core, event 
 import numpy as np
-import random 
+#import random 
 import math
 import time
 #import itertools
-import pickle
+#import pickle
 import pandas as pd
+import json
+import sys
+
 
 
 ############# functions #################
+
 # draw fixation cross function
 def draw_fixation(posit,lineSize,linecolor,linewidth): 
     
@@ -58,16 +62,13 @@ def uniq_trials(ecc):
     
     return trials,trgt_ecc,trgt_vf
 
-
-###################################
-
+# staircase function
 def staircase_1upDdown(D,response,step,max_val,min_val,curr_dist,counter):
 
     if response == 0: #if incorrect response
         if curr_dist < max_val: #and if the distance is not max value defined
             curr_dist = curr_dist + step # increase the flanker target separation by step
         counter = 1
-        reversal = 'True'
         
     else: #if correct response
         if counter == D: #if counted necessary number of responses (i.e. too easy)
@@ -75,63 +76,71 @@ def staircase_1upDdown(D,response,step,max_val,min_val,curr_dist,counter):
                 curr_dist = curr_dist - step #reduce distance between flanker and target
             counter = 1
         counter = counter + 1    
-        reversal = 'False'
         
-    return curr_dist,counter,reversal          
+    return curr_dist,counter        
             
     
 #######################################
-
-pp = '0' #raw_input("Participant number: ")
+    
+# define participant number and open json parameter file
+if len(sys.argv)<2:
+    raise NameError('Please add subject number (ex:01)'\
+                    'as 1st argument in the command line!')
+    
+elif len(sys.argv)<3:
+    raise NameError('Please add if running in ex:lab or Inesmac '\
+                    'as 2nd argument in the command line!')    
+else:
+    pp = str(sys.argv[1]).zfill(2) #fill subject number with 0 in case user forgets
+    
+    if sys.argv[2]=='Inesmac':
+        
+        with open('mac_parameters.json','r') as json_file:
+            params = json.load(json_file)
+            
+    elif sys.argv[2]=='lab':
+        with open('lab_parameters.json','r') as json_file:
+            params = json.load(json_file)
+    else:
+        raise NameError('Machine not defined, no parametes will be given') 
+        
 
 ########## Initial parameters #########
+        
 # general info
 num_blk = 2 #total number of blocks
-num_rep = 5#20 #number of repetions of unique display per block
-ecc_deg = [4,8,12]
+num_rep = 20 #number of repetions of unique display per block
 
-num_trl, _, _ = uniq_trials(ecc_deg) 
+num_trl, trgt_ecc,trgt_vf = uniq_trials(params['ecc']) 
 num_trl = num_trl*num_rep#total number of trials
 
 l_trl = r_trl = num_trl/2 #number of trials for left and right target locations
 
-stim_time = 1.5 #stimulus presentation time (seconds)
-iti = 0.5 #inter-trial-interval (seconds)
-
 # screen info   
-hRes = 1280#2560#1920 #900 
-vRes = 800#1600#1080 #700 
-screenHeight = 21.9#30 #height of the screen in cm 
-screenDis = 71    #distance between screen and chinrest in cm     
-backCol = 'black'
+# params['screenHeight'] - height of the screen in cm 
+# params['screenDis'] - distance between screen and chinrest in cm 
+# num_fl - number of distractors
+# initpos_fl - initial pos (degree)
+# ort_trgt - orientation of target (degrees)
+# max_dist - ratio of ecc that is max possible flk-trgt distance 
+# min_dist - ratio of ecc that is min possible flk-trgt distance
+# step_stair - step size for the staircase 
+# Down_factor - number of correct responses needed to increase difficulty
+ 
 
 # fixation cross info
 fixpos = (0,0) #center position
-fixlineSize = ang2pix(0.25,screenHeight,screenDis,vRes)
-fixcolor = 'white' 
-linewidth = ang2pix(0.05,screenHeight,screenDis,vRes) 
+fixlineSize = ang2pix(params['fixlineSize_deg'],params['screenHeight'],params['screenDis'],params['vRes'])
+linewidth = ang2pix(params['linewidth_deg'],params['screenHeight'],params['screenDis'],params['vRes']) 
 
 # gabor info
-siz_gab = ang2pix(1.5,screenHeight,screenDis,vRes) #size in deg of visual angle
-gab_sf = 6/ang2pix(1,screenHeight,screenDis,vRes) #sf cycles per pixel = sf_CyclesDeg / pixelsPerDegree
-sd_gab = ang2pix(0.06,screenHeight,screenDis,vRes) #standard deviation of gaussian
-num_fl = 2 # number of distractors
-dist_fl = 360/num_fl #distance between stim (degrees)
-initpos_fl = 90 #initial pos (degree)
+siz_gab = ang2pix(params['siz_gab_deg'],params['screenHeight'],params['screenDis'],params['vRes']) #size in deg of visual angle
+gab_sf = params['gab_sf_deg']/ang2pix(1,params['screenHeight'],params['screenDis'],params['vRes']) #sf cycles per pixel = sf_CyclesDeg / pixelsPerDegree
+sd_gab = ang2pix(params['sd_gab_deg'],params['screenHeight'],params['screenDis'],params['vRes']) #standard deviation of gaussian
+dist_fl = 360/params['num_fl'] #distance between stim (degrees)
 
-pos_fl = np.arange(initpos_fl,initpos_fl+360,dist_fl) #position of distractors (degrees), equally spaced
-ort_fl = np.repeat(0,num_fl) # all flankers have same orientation (0 equals vertical, goes clockwise until 360deg)
-ort_trgt = [5,355] #orientation of target (degrees)
-
-max_dist = 0.8 #ratio of ecc that is max possible flk-trgt distance 
-min_dist = 0.2 #ratio of ecc that is min possible flk-trgt distance
-step_stair = 0.05 #distance staircase step
-Down_factor = 3 #number of correct responses needed to increase difficulty
-trgt_fl_dist = max_dist #we start with max distance to make it easy
-
-
-#labels
-_, trgt_ecc,trgt_vf = uniq_trials(ecc_deg) 
+pos_fl = np.arange(params['initpos_fl'],params['initpos_fl']+360,dist_fl) #position of distractors (degrees), equally spaced
+ort_fl = np.repeat(0,params['num_fl']) # all flankers have same orientation (0 equals vertical, goes clockwise until 360deg)
 
 
 trls_idx = np.repeat(range(0,num_trl/num_rep),num_rep) #range of indexes for all trials 
@@ -143,10 +152,12 @@ RT_trl = np.array(np.zeros((num_blk,num_trl)),object) #array for all RTs
 key_trl = np.array(np.zeros((num_blk,num_trl)),object) #array for all key presses
 display_idx = np.array(np.zeros((num_blk,num_trl)),object) #array for idx of all displays
 trgt_ort_lbl = np.array(np.zeros((num_blk,num_trl)),object) #array for target orientations
+distances = np.array(np.zeros((num_blk,num_trl)),object) #array for all distance values
+
 
 # create a window
 #win = visual.Window(size=(hRes, vRes), color = backCol, units='pix',fullscr  = True, screen = 1,allowStencil=True)
-win = visual.Window(size= (hRes, vRes), color = backCol, units='pix',fullscr  = True, screen = 0,allowStencil=True)   
+win = visual.Window(size= (params['hRes'], params['vRes']), color = params['backCol'], units='pix',fullscr  = True, screen = 0,allowStencil=True)   
    
 #pause
 core.wait(2.0)
@@ -157,17 +168,16 @@ text2 = 'Press spacebar to start'
 PressText = visual.TextStim(win, text=text2, color='white', height=30, pos = (0,-140))
     
 BlockText.draw()
-draw_fixation(fixpos,fixlineSize,fixcolor,linewidth) #draw fixation 
+draw_fixation(fixpos,fixlineSize,params['fixcolor'],linewidth) #draw fixation 
 PressText.draw()
 win.flip()
 event.waitKeys(keyList = 'space') 
-
-core.wait(2.0)
 
 
 for j in range(num_blk):
     
     counter = 1 #staircase counter
+    trgt_fl_dist = params['max_dist'] #we start with max distance to make it easy
     
     np.random.shuffle(ort_lbl) #randomize target orientation
     np.random.shuffle(trls_idx) #randomize index for display
@@ -181,79 +191,82 @@ for j in range(num_blk):
     PressText = visual.TextStim(win, text=text2, color='white', height=30, pos = (0,-140))
     
     BlockText.draw()
-    draw_fixation(fixpos,fixlineSize,fixcolor,linewidth) #draw fixation 
+    draw_fixation(fixpos,fixlineSize,params['fixcolor'],linewidth) #draw fixation 
     #trgt.draw()
     PressText.draw()
     win.flip()
     event.waitKeys(keyList = 'space') 
 
-    draw_fixation(fixpos,fixlineSize,fixcolor,linewidth) #draw fixation 
+    draw_fixation(fixpos,fixlineSize,params['fixcolor'],linewidth) #draw fixation 
     win.flip() # flip the screen
     core.wait(2.0) #pause
     
     for k in range(num_trl):
         
-        ort_trl = ort_trgt[0] if ort_lbl[k]=='right' else ort_trgt[1] #define target orientation for trial
+        ort_trl = params['ort_trgt'][0] if ort_lbl[k]=='right' else params['ort_trgt'][1] #define target orientation for trial
 
         if trgt_vf[trls_idx[k]] == 'left':
-            xpos_trgt = -ang2pix(float(trgt_ecc[trls_idx[k]]),screenHeight,screenDis,vRes)
+            xpos_trgt = -ang2pix(float(trgt_ecc[trls_idx[k]]),params['screenHeight'],params['screenDis'],params['vRes'])
             trgt = visual.GratingStim(win=win,tex='sin',mask='gauss',maskParams={'sd': sd_gab},ori=ort_trl,sf=gab_sf,size=siz_gab,pos=(xpos_trgt,0))           
         else:
-            xpos_trgt = ang2pix(float(trgt_ecc[trls_idx[k]]),screenHeight,screenDis,vRes)
+            xpos_trgt = ang2pix(float(trgt_ecc[trls_idx[k]]),params['screenHeight'],params['screenDis'],params['vRes'])
             trgt = visual.GratingStim(win=win,tex='sin',mask='gauss',maskParams={'sd': sd_gab},ori=ort_trl,sf=gab_sf,size=siz_gab,pos=(xpos_trgt,0))            
              
         trgt.draw()
         
         for i in range(len(pos_fl)):
-            xpos_fl,ypos_fl = pol2cart(ang2pix(float(trgt_ecc[trls_idx[k]])*float(trgt_fl_dist),screenHeight,screenDis,vRes), pos_fl[i])
+            xpos_fl,ypos_fl = pol2cart(ang2pix(float(trgt_ecc[trls_idx[k]])*float(trgt_fl_dist),params['screenHeight'],params['screenDis'],params['vRes']), pos_fl[i])
             flank = visual.GratingStim(win=win,tex='sin',mask='gauss',maskParams={'sd': sd_gab},ori=ort_fl[i],sf=gab_sf,size=siz_gab,pos=(xpos_fl+xpos_trgt,ypos_fl))
             flank.draw()
 
     
-        draw_fixation(fixpos,fixlineSize,fixcolor,linewidth) #draw fixation
+        draw_fixation(fixpos,fixlineSize,params['fixcolor'],linewidth) #draw fixation
         win.flip() # flip the screen
         
         t0 = core.getTime() #get the time (seconds)
         key = [] # reset key to nothing 
            
-        while core.getTime() - t0 < stim_time:
+        while core.getTime() - t0 < params['stim_time']: # while current time < stimulus presentation time (seconds)
             
-            key = event.getKeys(keyList = ['left','right'])
+            key = event.getKeys(keyList = ['left','right','s'])
             
             if len(key)>0:
+                if key[0] == 's': #stop key
+                    win.close()
+                    core.quit()
+                    break 
+                
                 RT_trl[j][k] = core.getTime() - t0 
                 key_trl[j][k] = key[0] 
-                time.sleep(stim_time-(core.getTime() - t0)) 
+                time.sleep(params['stim_time']-(core.getTime() - t0)) 
                 break
          
         if key_trl[j][k] == ort_lbl[k]:
             response = 1
-            print 'correct' 
         else:
             response = 0
-            print 'wrong'
             
-        trgt_fl_dist,counter,_ = staircase_1upDdown(Down_factor,response,step_stair,max_dist,min_dist,trgt_fl_dist,counter)
+        trgt_fl_dist,counter = staircase_1upDdown(params['Down_factor'],response,params['step_stair'],params['max_dist'],params['min_dist'],trgt_fl_dist,counter)
+        print 'response is %d and distance is %.2f' % (response, trgt_fl_dist)
+        distances[j][k] = trgt_fl_dist
+
     
-        draw_fixation(fixpos,fixlineSize,fixcolor,linewidth) #draw fixation 
+        draw_fixation(fixpos,fixlineSize,params['fixcolor'],linewidth) #draw fixation 
         win.flip() # flip the screen
-        core.wait(iti) #pause
+        core.wait(params['iti']) #pause
 
     display_idx[j][:] = trls_idx
     trgt_ort_lbl[j][:] = ort_lbl
 
 
 
-
 # save relevant variables in panda dataframe
 for d in range(num_blk):
     target_ecc = np.zeros((1,num_trl))
-    target_flank_dist= np.zeros((1,num_trl))
     for l in range(num_trl):
-        target_ecc[0][l] = trgt_ecc[display_idx[0][l]];
-        #target_flank_dist[0][l] = trgt_fl_dist[display_idx[0][l]];
+        target_ecc[0][l] = trgt_ecc[display_idx[0][l]]
     
-    dict_var = {'target_orientation':trgt_ort_lbl[d][:], 'target_ecc':target_ecc[0][:], 'target_flank_ratio':target_flank_dist[0][:],'key_pressed':key_trl[d][:],'RT':RT_trl[d][:]}
+    dict_var = {'target_orientation':trgt_ort_lbl[d][:], 'target_ecc':target_ecc[0][:], 'target_flank_ratio':distances[d][:],'key_pressed':key_trl[d][:],'RT':RT_trl[d][:]}
     if d==0:
         df = pd.DataFrame(data=dict_var)
     else:
