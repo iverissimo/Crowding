f42d5362fff8e80fba3efc9ba2b8dcdbe5880693
iverissimo <verissimo.iness@gmail.com>
2020-01-17 15:47:32 +0100

    focusing on interesting plots, need to add more eyetracking analysis

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -59,6 +59,8 @@ crwd_edf = [x for _,x in enumerate(os.listdir(output_crwd)) if x.endswith('.EDF'
 vs_csv = [x for _,x in enumerate(os.listdir(output_vs)) if x.endswith('.csv')]; vs_csv.sort()
 vs_edf = [x for _,x in enumerate(os.listdir(output_vs)) if x.endswith('.EDF')]; vs_edf.sort() 
 
+# p-value thresth
+p_value = 0.05
 
 # loop over subjects
 
@@ -211,8 +213,11 @@ for j in range(len(all_subs)):
         test_fix_ecc_vs.append(meanfix_ecc(df_vs,eye_data,ecc))
         test_fix_set_vs.append(meanfix_setsize(df_vs,eye_data,analysis_params['set_size']))
 
+# PLOTS
 
-# plot accuracy for crowding
+## CROWDING ####
+
+# ACCURACY CROWDING WITH AND WITHOUT FLANKERS
 fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 plt.subplot(1,2,1)
 plt.hist(np.array(test_acc_fl))
@@ -229,13 +234,12 @@ plt.xlim(0.5,1)
 fig.savefig(os.path.join(plot_dir,'crowding_accuracy_hist_%d-subs.svg'%len(test_subs)), dpi=100)
 
 
+# SAME BUT WITH BOXPLOTS, AND TESTING DISTRIBUTION DIFFERENCE
+
 # wilcoxon test
 # non-parametric statistical hypothesis test used to compare two matched samples
 # to assess whether their population mean ranks differ
 pval_acc_crwd = wilcoxon(np.mean(np.array(test_acc_fl),axis=-1), np.mean(np.array(test_acc_nofl),axis=-1))[-1]
-
-#diff_acc = np.mean(np.array(acc_nofl),axis=-1)-np.mean(np.array(acc_fl),axis=-1)
-#wilcoxon(diff_acc)
 
 crwd_acc4plot = pd.DataFrame(data=np.array([np.mean(np.array(test_acc_fl),axis=-1),
                                 np.mean(np.array(test_acc_nofl),axis=-1)]).T, 
@@ -249,17 +253,21 @@ y, h, col = 1, .025, 'k'
 plt.plot([0, 0, 1, 1], [y, y+h, y+h, y], lw=1.5, c=col)
 plt.text(.5, y+h+.01, 'p-val = %.3f'%pval_acc_crwd, ha='center', va='bottom', color=col)
 plt.ylim(0.5, 1.1)
+plt.ylabel('Accuracy')
+plt.xlabel('')
+plt.title('Accuracy crowding (with and without flankers)')
+
+if pval_acc_crwd<p_value:
+    print('Significant difference in accuracy distribution \nbetween flanker and no flanker \nacross ecc (p-val = %.6f)'%pval_acc_crwd)
 
 fig.savefig(os.path.join(plot_dir,'crowding_mean_accuracy_boxplot_wilcoxtest_%d-subs.svg'%len(test_subs)), dpi=100)
 
-# do same but now per ecc
+
+# SAME BUT PER ECC
 
 for g,_ in enumerate(ecc):
     pval_acc_crwd = wilcoxon(np.array(test_acc_fl)[...,g], np.array(test_acc_nofl)[...,g])[-1]
-    print('p-value is %0.10f'%pval_acc_crwd)
-    #diff_acc = np.mean(np.array(acc_nofl),axis=-1)-np.mean(np.array(acc_fl),axis=-1)
-    #wilcoxon(diff_acc)
-
+    
     crwd_acc4plot = pd.DataFrame(data=np.array([np.array(test_acc_fl)[...,g],
                                     np.array(test_acc_nofl)[...,g]]).T, 
                                  columns = ['mean_acc_fl','mean_acc_nofl'])
@@ -273,27 +281,16 @@ for g,_ in enumerate(ecc):
     plt.text(.5, y+h+.01, 'p-val = %.6f'%pval_acc_crwd, ha='center', va='bottom', color=col)
     plt.ylim(0.5, 1.1)
     plt.title('%d ecc accuracy crowding (with and without flankers)'%ecc[g])
+    plt.ylabel('Accuracy')
+    plt.xlabel('')
+    
+    if pval_acc_crwd<p_value:
+        print('Significant difference in accuracy distribution \nbetween flanker and no flanker \nfor %d ecc (p-val = %.6f)'%(ecc[g],pval_acc_crwd))
+    
     fig.savefig(os.path.join(plot_dir,'crowding_%decc_accuracy_boxplot_wilcoxtest_%d-subs.svg'%(ecc[g],len(test_subs))), dpi=100)
 
 
-# plot distribution of critical spacing values for crowding
-fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
-plt.subplot(1,2,1)
-plt.hist(np.array(test_all_cs))
-plt.title('Critical distance per eccentricity°')
-plt.xlim(0.2,0.8)
-plt.legend(['4°','8°','12°'])
-
-plt.subplot(1,2,2)
-plt.hist(np.array(test_mean_cs))
-plt.title('Critical distance across eccentricities')
-plt.xlim(0.2,0.8)
-
-fig.savefig(os.path.join(plot_dir,'crowding_criticaldistance_hist_%d-subs.svg'%len(test_subs)), dpi=100)
-
-
-# plot change in critical spacing value 
-# weigthed by accuracy 
+# MEAN CRITICAL SPACING, WEIGHTED BY ACCURACY, PER ECC
 fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 weighted_cs_mean = []
 weighted_cs_std = []
@@ -309,7 +306,8 @@ for w in range(len(ecc)):
     
     crwd_df4plot = crwd_df4plot.append(pd.DataFrame({'ecc': np.tile(ecc[w],len(cs_ecc)),
                                   'cs':cs_ecc,
-                                   'acc':acc_ecc}),ignore_index=True)
+                                   'acc':acc_ecc,
+                                    'sub':np.array(test_subs)}),ignore_index=True)
 
 plt.errorbar(ecc, weighted_cs_mean, yerr=weighted_cs_std)
 plt.ylabel('Critical spacing',fontsize=18)
@@ -317,20 +315,29 @@ plt.xlabel('ecc',fontsize=18)
 plt.title('Mean critical spacing, weighted by accuracy',fontsize=18)
 fig.savefig(os.path.join(plot_dir,'crowding_meanCS-weighted_errorbar_%d-subs.svg'%len(test_subs)), dpi=100)
 
+# SHOW INDIVIDUAL SUB CS DISTRIBITUION PER ECC
+fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 
-# boxplots with mean CS per ecc
+with sns.color_palette("pastel", len(test_subs)):
+    sns.catplot(x="ecc", y="cs", hue="sub", kind="point", data=crwd_df4plot)
+plt.title('Critical Spacing, per ecc')
+#fig.savefig(os.path.join(plot_dir,'crowding_CS_ecc_individual_%d-subs.svg'%len(test_subs)), dpi=100)
+
+#sns.lineplot(x='ecc', y='cs', data=crwd_df4plot)
+
+# BOXPLOTS WITH CS PER ECC
 fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 sns.boxplot(x='ecc', y='cs', data=crwd_df4plot)
 sns.swarmplot(x='ecc', y='cs', data=crwd_df4plot,color=".25")
 fig.savefig(os.path.join(plot_dir,'crowding_meanCS-ecc-weighted_boxplot_%d-subs.svg'%len(test_subs)), dpi=100)
 
-# do kruskal wallis to see if averages are different 
-# The Kruskal-Wallis H-test tests the null hypothesis that 
-# the population median of all of the groups are equal. It is a non-parametric version of ANOVA.
+# do Friedman to see if averages are different 
+# The Friedman test tests the null hypothesis that repeated measurements of the same individuals have the same distribution. 
+#It is often used to test for consistency among measurements obtained in different ways.
 
-pkruskal = kruskal(np.array(test_all_cs)[...,0], np.array(test_all_cs)[...,1], np.array(test_all_cs)[...,2])[-1]
-if pkruskal<0.05:
-    print('CS between ecc is different, kruskal-wallis with p-value = %.6f'%pkruskal)
+pfriedman = friedmanchisquare(np.array(test_all_cs)[...,0], np.array(test_all_cs)[...,1], np.array(test_all_cs)[...,2])[-1]
+if pfriedman<p_value:
+    print('CS between ecc is different, friedman with p-value = %.6f'%pfriedman)
 else:
     print('CS between ecc is the same')
 
@@ -342,22 +349,23 @@ for d in range(len(ecc_compare)):
     pval_wilcox = wilcoxon(np.array(test_all_cs)[...,np.where(np.array(ecc)==ecc_compare[d][0])[0][0]], 
                            np.array(test_all_cs)[...,np.where(np.array(ecc)==ecc_compare[d][1])[0][0]])[-1]
     
-    if pval_wilcox<(0.05/3): # bonferroni correction of p-value, right?
+    if pval_wilcox<(p_value/3): # bonferroni correction of p-value, right?
         print('SIGNIFICANT difference (p-val %.6f) in CS of ecc pair%s'%(pval_wilcox,str(ecc_compare[d])))
-
 
 # kolmogorov smir test to see if sample of mean cs (across ecc)
 # follows normal distribution - if p<0.05 not normally distributed
 pval_crwd_norm = kstest(test_mean_cs, 'norm')[-1]
-if pval_crwd_norm<0.05:
-    print('mean critical spacing values, across ecc NOT normally distributed (KS with p-value=%f)'%pval_crwd_norm)
+if pval_crwd_norm<p_value:
+    print('mean critical spacing values, across ecc NOT normally distributed (KS with p-value=%.6f)'%pval_crwd_norm)
 else:
-    print('mean critical spacing values, across ecc normally distributed (KS with p-value=%f)'%pval_crwd_norm)
+    print('mean critical spacing values, across ecc normally distributed (KS with p-value=%.6f)'%pval_crwd_norm)
 
 
 print('Now look at visual search data')
 
-# plot accuracy for visual search
+# VISUAL SEARCH
+
+# ACCURACY VISUAL SEARCH
 fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 
 plt.subplot(1,2,1)
@@ -375,7 +383,7 @@ plt.legend(['4°','8°','12°'])
 fig.savefig(os.path.join(plot_dir,'search_accuracy_RT_hist_%d-subs.svg'%len(test_subs)), dpi=100)
 
 
-# plot mean Reaction times as a function of eccentricity
+# RT VS ECC
 fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 
 rt_ecc_vs4plot = pd.DataFrame([])
@@ -390,7 +398,7 @@ ax.set_title('ecc vs RT %d subs'%len(test_subs))
 plt.savefig(os.path.join(plot_dir,'search_ecc_RT_regression_%d-subs.svg'%len(test_subs)), dpi=100,bbox_inches = 'tight')
  
 
-# plot mean Reaction times as a function of eccentricity
+# RT VS SET SIZE
 fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 
 rt_set_vs4plot = pd.DataFrame([])
@@ -401,20 +409,19 @@ for k in range(len(analysis_params['set_size'])):
 ax = sns.lmplot(x='set', y='RT',data=rt_set_vs4plot)
 ax.set(xlabel='set size', ylabel='RT [s]')
 ax = plt.gca()
-ax.set_title('set size vs RT plot, %d subs'%len(test_subs))
+ax.set_title('set size vs RT %d subs'%len(test_subs))
 plt.savefig(os.path.join(plot_dir,'search_setsize_RT_regression_%d-subs.svg'%len(test_subs)), dpi=100,bbox_inches = 'tight')
  
 
-# inverse efficiency score
+# INVERSE EFFICIENCY SCORE
 inv_eff = np.array(test_rt_ecc_vs)/np.array(test_acc_vs_ecc)
 inv_eff_mean = np.mean(inv_eff,axis=0)
-#print(inv_eff)
 print('mean inverse efficiency score, per ecc is %s'%str(inv_eff_mean))
 
 
-# look at eye tracking data
+# EYETRACKING FOR VISUAL SEARCH
 
-# Number of fixations as a function of eccentricity
+# NUMBER OF FIXATIONS VS ECC
 fig= plt.figure(num=None, figsize=(15,7.5), dpi=100, facecolor='w', edgecolor='k')
 
 fix_ecc_vs4plot = pd.DataFrame([])
@@ -425,7 +432,7 @@ for k in range(len(ecc)):
 ax = sns.lmplot(x='ecc', y='fix',data=fix_ecc_vs4plot)
 ax.set(xlabel='eccentricity [dva]', ylabel='# fixations')
 ax = plt.gca()
-ax.set_title('ecc vs number of fixations plot, all subs')
+ax.set_title('ecc vs number of fixations %d subs'%len(test_subs))
 ax.axes.set_ylim(0,)
 plt.savefig(os.path.join(plot_dir,'search_ecc_numfix_regression_%d-subs.svg'%len(test_subs)), dpi=100,bbox_inches = 'tight')
  
@@ -441,7 +448,7 @@ for k in range(len(analysis_params['set_size'])):
 ax = sns.lmplot(x='set', y='fix',data=fix_set_vs4plot)
 ax.set(xlabel='set size', ylabel='number fixations')
 ax = plt.gca()
-ax.set_title('set size vs number fixations plot, all subs')
+ax.set_title('set size vs number fixations %d subs'%len(test_subs))
 plt.savefig(os.path.join(plot_dir,'search_setsize_fix_regression_%d-subs.svg'%len(test_subs)), dpi=100,bbox_inches = 'tight')
  
 
